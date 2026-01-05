b04e0e9bd020b1f9ae20b1320359560a1f4a244a
alix-jourdain <alix.jourdain@edu.esiee.fr>
2026-01-05 15:35:30 +0100

    Implement history and back command functions
    Add history and back command functions to actions.py

--- actions.py
+++ actions.py
@@ -137,3 +137,21 @@ class Actions:
             print("\t- " + str(command))
         print()
         return True
+    def history(game, list_of_words, number_of_parameters):
+        l = len(list_of_words)
+        if l != number_of_parameters + 1:
+            command_word = list_of_words[0]
+            print(MSG0.format(command_word=command_word))
+            return False
+
+        print("\n" + game.player.get_history())
+        return True
+        def back(game, list_of_words, number_of_parameters):
+            l = len(list_of_words)
+            if l != number_of_parameters + 1:
+                command_word = list_of_words[0]
+                print(MSG0.format(command_word=command_word))
+                return False
+
+            return game.player.back()
+
