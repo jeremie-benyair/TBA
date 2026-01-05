9457cba1f77b4aee0597a313410a9d15b225cc03
jeremie-benyair <jeremie.benyair@edu.esiee.fr>
2026-01-05 15:32:41 +0100

    Create Item class with initialization and string representation

---
+++ item.py
@@ -0,0 +1,7 @@
+Class Item():
+def __init__(self,name,description,weight):
+    self.name=name
+    self.description=description
+    self.weight=weight
+def __str__(self):
+    return f"{self.name} : {self.description} ({self.weight}) kg)\n"
