c21a1021b65afcd0d734c0b77f8962d5b6d7f112
alix-jourdain <alix.jourdain@edu.esiee.fr>
2026-01-05 15:38:37 +0100

    Refactor back function indentation in actions.py

--- actions.py
+++ actions.py
@@ -146,12 +146,12 @@ class Actions:
 
         print("\n" + game.player.get_history())
         return True
-        def back(game, list_of_words, number_of_parameters):
-            l = len(list_of_words)
-            if l != number_of_parameters + 1:
-                command_word = list_of_words[0]
-                print(MSG0.format(command_word=command_word))
-                return False
+    def back(game, list_of_words, number_of_parameters):
+        l = len(list_of_words)
+        if l != number_of_parameters + 1:
+            command_word = list_of_words[0]
+            print(MSG0.format(command_word=command_word))
+            return False
 
-            return game.player.back()
+        return game.player.back()
 
