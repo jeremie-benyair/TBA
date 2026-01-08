a2acb569cdb6c41736fca34fff8c2bb62416b550
jeremie-benyair <jeremie.benyair@edu.esiee.fr>
2026-01-08 23:25:34 +0100

    Update actions.py

--- actions.py
+++ actions.py
@@ -203,6 +203,18 @@ class Actions:
             print(f"L'item a été retiré de votre inventaire et déposé sur le sol.\n")
         return True
         
+    def check(game,list_of_words,number_of_parameters):
+        l=len(list_of_words)
+        if l!=number_of_parameters + 1:
+            command_word=list_of_words[0]
+            print(MSG0.format(command_word=command_word))
+            return False
+        
+        if game.player.inventory=={}:
+                print("Aucun objet n'est présent dans votre inventaire.\n")
+        else:
+            print("Vous avez actuellement dans votre inventaire : \n")
+            
             
 
             
