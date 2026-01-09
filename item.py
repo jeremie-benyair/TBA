eaff38d8b5697a09a2cd596a5f56b9a50ab95df1
jeremie-benyair <jeremie.benyair@edu.esiee.fr>
2026-01-09 14:48:31 +0100

    Enhance Item constructor with optional parameters
    Add optional parameters 'text' and 'damage' to Item class.

--- item.py
+++ item.py
@@ -1,7 +1,9 @@
 Class Item():
-def __init__(self,name,description,weight):
+def __init__(self,name,description,weight,text=None,damage=None):
     self.name=name
     self.description=description
     self.weight=weight
+    self.text=text if text is not None
+    self.damage=damage if damage is not None
 def __str__(self):
     return f"{self.name} : {self.description} ({self.weight}) kg)\n"
