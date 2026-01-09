93edffeaa28b3a3073963ab2173780f429a8f675
jeremie-benyair <jeremie.benyair@edu.esiee.fr>
2026-01-09 14:50:25 +0100

    Update item.py

--- item.py
+++ item.py
@@ -3,7 +3,7 @@ def __init__(self,name,description,weight,text=None,damage=None):
     self.name=name
     self.description=description
     self.weight=weight
-    self.text=text if text is not None else None
-    self.damage=damage if damage is not None else None
+    self.text=text 
+    self.damage=damage
 def __str__(self):
     return f"{self.name} : {self.description} ({self.weight}) kg)\n"
