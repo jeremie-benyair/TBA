268b63537436948962583aa64237e33a1eba48da
jeremie-benyair <jeremie.benyair@edu.esiee.fr>
2026-01-11 14:56:04 +0100

    Update game.py

--- game.py
+++ game.py
@@ -39,6 +39,7 @@ class Game:
         self.commands["drop"]=drop
         check=Command("check"," : jeter un oeil à son inventaire",Actions.check,0)
         self.commands["check"] = check 
+        use=Command["use","utiliser l'objet équipé.",Actions.use,0)
 
 
         
