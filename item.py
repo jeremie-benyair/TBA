6a2a637cbacf616872592ebfcdd6a5e0dee1c9fd
jeremie-benyair <jeremie.benyair@edu.esiee.fr>
2026-01-12 08:47:28 +0100

    Update item.py

--- item.py
+++ item.py
@@ -1,10 +1,10 @@
-Class Item():
-def __init__(self,name,description,weight,text=None,damage=None,type=None):
-    self.name=name
-    self.description=description
-    self.weight=weight
-    self.text=text 
-    self.damage=damage
-    self.type=type
+class Item():
+    def __init__(self,name,description,weight,text=None,damage=None,type=None):
+        self.name=name
+        self.description=description
+        self.weight=weight
+        self.text=text 
+        self.damage=damage
+        self.type=type
 def __str__(self):
     return f"{self.name} : {self.description} ({self.weight}) kg)\n"
