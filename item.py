1097a6688941a0a95769e9c1cbc89d57742ab036
jeremie-benyair <jeremie.benyair@edu.esiee.fr>
2026-01-14 17:06:04 +0100

    Fix constructor method in Flashlight class

--- item.py
+++ item.py
@@ -9,8 +9,8 @@ class Item():
     def __str__(self):
         return f"{self.name} : {self.description} ({self.weight}) kg)\n"
 class Flashlight(Item):
-    def ___init___(self,name,description,weight):
-        super().___init___(name,description,weight)
+    def __init__(self,name,description,weight):
+        super().__init__(name,description,weight)
     def state(self,game):
         self.on= not self.on
         if self.on:
