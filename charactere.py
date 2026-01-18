e2f5869c3008cde94ce7025aa9e710bc1b6333ed
alix-jourdain <alix.jourdain@edu.esiee.fr>
2026-01-18 18:03:59 +0100

    Implement message retrieval and cycling in character
    Add message cycling functionality to character class

--- charactere.py
+++ charactere.py
@@ -4,6 +4,8 @@ class Charactere:
         self.description = description
         self.current_room = current_room
         self.msgs = msgs
+        self.msg_index = 0
+
 
     def __str__(self):
         return f"{self.name} : {self.description} (Salle : {self.current_room.name}) (Messages : {self.msgs})\n"
@@ -25,3 +27,15 @@ class Charactere:
         self.current_room = next_room
 
         return True
+    def get_msg(self):
+        if not self.msgs:
+            return f"{self.name} n'a rien à dire."
+
+        # Retire et récupère le premier message
+        msg = self.msgs.pop(0)
+
+        # Le remet à la fin pour créer un cycle
+        self.msgs.append(msg)
+
+        return msg
+
