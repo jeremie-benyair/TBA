a869129ca91c2419ac857f771b415fa5aee5317b
jeremie-benyair <jeremie.benyair@edu.esiee.fr>
2026-01-20 19:06:24 +0100

    Update game.py

--- game.py
+++ game.py
@@ -180,6 +180,7 @@ class Game:
         chambre_1.inventory["batte"] = batte
         bible=Bible("bible","une vieille bible étrange qui mériterait qu'on l'examine.",0.3)
         eglise.inventory["bible"]=bible
+        key_cinema=Key("clé du cinéma"," ",0.1)
 
 
         
