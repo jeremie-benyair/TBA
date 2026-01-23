46874df603e386890ea775a8faeee94cfec26ece
jeremie-benyair <jeremie.benyair@edu.esiee.fr>
2026-01-23 22:06:57 +0100

    Update player.py

--- player.py
+++ player.py
@@ -9,6 +9,7 @@ class Player():
         self.inventory={}
         self.equipped_item= None
         self.max_weight=2
+        self.rewards = []
         self.quest_manager = QuestManager(self)
     def get_history(self):
         if not self.historique:
