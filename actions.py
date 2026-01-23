0c52c80906038674d2f87cab6db8ab2f62568b21
jeremie-benyair <jeremie.benyair@edu.esiee.fr>
2026-01-24 00:20:27 +0100

    Update actions.py

--- actions.py
+++ actions.py
@@ -281,9 +281,9 @@ class Actions:
             if pnj.name.lower() == target_name.lower():
                 print(pnj.get_msg())
                 return True
-            if not pnj.has_spoken:
-                pnj.has_spoken = True 
-                game.player.quest_manager.activate_quest("Retrouver le doudou")
+                if not pnj.has_spoken:
+                    pnj.has_spoken = True 
+                    game.player.quest_manager.activate_quest("Retrouver le doudou")
                 return True
                 
                 
