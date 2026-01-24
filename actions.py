f52f1f0ae4b2c05c729b9b707db892344fe4fc27
alix-jourdain <alix.jourdain@edu.esiee.fr>
2026-01-24 12:28:08 +0100

    Implement weight check for inventory items
    Add weight calculation for items being added to inventory

--- actions.py
+++ actions.py
@@ -202,6 +202,16 @@ class Actions:
             command_word = list_of_words[0]
             print(MSG1.format(command_word=command_word))
             return False
+        # Calcul du poids total si on ajoute cet objet 
+        poids_total = game.player.total_weight() 
+        poids_objet = item.weight 
+        # Si c'est un medkit, on ajoute juste le poids d'un seul exemplaire 
+        from item import Medkit 
+        if isinstance(item, Medkit): 
+            poids_objet = item.weight # 1 seul medkit à la fois 
+        if poids_total + poids_objet > game.player.max_weight: 
+            print("Vous ne pouvez pas prendre cet objet : votre inventaire est plein.\n") 
+            return False
     
         objet = list_of_words[1]
     
