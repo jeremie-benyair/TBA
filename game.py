40a4381c203d48ca287230e47beb1e66d2d9afa9
jeremie-benyair <jeremie.benyair@edu.esiee.fr>
2026-01-25 14:51:54 +0100

    Update game.py

--- game.py
+++ game.py
@@ -315,6 +315,14 @@ class Game:
                 if "Indice : 05" not in self.player.rewards: 
                     self.player.add_reward("Indice : 05")
                     print("🎁 Récompense : Indice : 05\n")
+            if self.player.quest_manager.win():
+                print("\n Toutes les quêtes ont été accomplies !")
+                print(" Une silhouette familière t’attend dans l’obscurité de la cave...")
+                print(" « Tu m’as sauvé… » murmure ta femme en larmes.")
+                print(" Vous vous enlacez dans un silence chargé d’émotion.")
+                print("🏁 Fin du jeu.")
+                self.finished = True
+
 
 
 
