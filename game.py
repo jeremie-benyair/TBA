2deb334ce6e873167d44ffb4d348f5978f669e85
jeremie-benyair <jeremie.benyair@edu.esiee.fr>
2026-01-26 18:22:12 +0100

    Update game.py

--- game.py
+++ game.py
@@ -424,7 +424,7 @@ class Game:
 
 
     def print_welcome(self):
-         print(f"\nBienvenue {self.player.name} dans ce jeu d'aventure/horreur textuel.\n" 
+        print(f"\nBienvenue {self.player.name} dans ce jeu d'aventure/horreur textuel.\n" 
                                          "Vous venez d’arriver à Silent Hil,une ville abandonnée, autrefois paisible, aujourd’hui rongée par le silence et la peur.\n" 
                                          "Les rues sont désertes, les bâtiments à l’abandon, les habitants ont disparu ou ont laissé place à quelque chose de bien plus inquiétant.\n" 
                                          "C’est ici que votre femme a été vue pour la dernière fois. Depuis, plus aucun signe d’elle.\n" 
